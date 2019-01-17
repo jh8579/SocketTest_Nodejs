004fe4866138d93d73fe53e32fecfcf44edf1bb6
unknown <jh8579@gmail.com>
2019-01-17 15:53:32 +0900

    dd

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ console.log("socket server connected 3000 port")
 
 // connection이 발생할 때 핸들러를 실행한다.
 io.sockets.on('connection', function (socket) {
+    var header = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
+    //var header1 = client.handshake.headers['x-forwarded-for'] || client.handshake.address.address;
+
     console.log('user connected');
+    console.log(socket.handshake.headers)
 
     // 클라이언트로 news 이벤트를 보낸다.
     socket.emit('news', { hello: 'world' }), function(){
