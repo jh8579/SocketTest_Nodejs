e884e9c32b7f7ef16dcbac8bd5a65f213e98ab68
unknown <jh8579@gmail.com>
2019-01-17 14:43:21 +0900

    First commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var io = require('socket.io').listen(3000);
 // connection이 발생할 때 핸들러를 실행한다.
 io.sockets.on('connection', function (socket) {  
 // 클라이언트로 news 이벤트를 보낸다.
+    console.log("Connected");
     socket.emit('news', { hello: 'world' });
 
 // 클라이언트에서 my other event가 발생하면 데이터를 받는다.
