87f2e9b03aed8c56d96e5d599667c094d4a5b222
unknown <jh8579@gmail.com>
2019-01-17 15:43:26 +0900

    dd

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // 3000 포트로 소켓을 연다
 var io = require('socket.io').listen(3000);
+console.log("socket server connected 3000 port")
 
 // connection이 발생할 때 핸들러를 실행한다.
-io.sockets.on('connection', function (socket) {  
+io.sockets.on('connection', function (socket) {
+    console.log('user connected');
+
     // 클라이언트로 news 이벤트를 보낸다.
-    socket.emit('news', { hello: 'world' });
-    console.log("Connected");
+    socket.emit('news', { hello: 'world' }), function(){
+        console.log('sending')
+    };
 
     // 클라이언트에서 my other event가 발생하면 데이터를 받는다.
     socket.on('my other event', function (data) {  
         console.log(data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
 });
